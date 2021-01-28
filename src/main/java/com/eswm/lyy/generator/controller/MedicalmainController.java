c864d0c573ff920a30c335d78e9fec8165a6a6d2
于文静 <fbiwenjingyu@163.com>
2021-01-28 14:37:21 +0800

    时间格式处理

--- src/main/java/com/eswm/lyy/generator/controller/MedicalmainController.java
+++ src/main/java/com/eswm/lyy/generator/controller/MedicalmainController.java
@@ -10,11 +10,7 @@ import com.eswm.lyy.generator.entity.*;
 import com.eswm.lyy.generator.service.*;
 import com.eswm.lyy.generator.utils.R;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.eswm.lyy.generator.utils.PageUtils;
 
@@ -109,6 +105,7 @@ public class MedicalmainController {
      * @param barCode
      * @return
      */
+    @CrossOrigin
     @RequestMapping("/queryByBarCode")
     public R queryByBarCode(@RequestParam String barCode){
         JSONObject data = new JSONObject();
